785107c99c045744c09aadd494c91f87d7aa9179
Amy <amy.jeanette.morrson@gmail.com>
2017-12-15 13:29:41 +0000

    added tag model

---
+++ models/tag.rb
@@ -0,0 +1,28 @@
+require('./db/sql_runner')
+
+class Tag
+
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO tags (name) VALUES ($1) RETURNING id;"
+    values = [@name]
+    tag = SqlRunner.run(sql, values)
+    @id = tag[0]['id'].to_i
+  end
+
+  def self.delete_all()
+      sql = "DELETE FROM tags;"
+      SqlRunner.run(sql)
+  end
+
+  def self.all()
+      sql = "SELECT * FROM tags;"
+      merchants = SqlRunner.run_sql_and_map(sql, 'Tag')
+  end
+
+end
