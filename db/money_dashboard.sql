535b7e128d254b2b724badc496956e699b98a452
Amy <amy.jeanette.morrson@gmail.com>
2017-12-15 13:05:47 +0000

    sql created tables

---
+++ db/money_dashboard.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS merchants;
+
+CREATE TABLE merchants (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE tags (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id SERIAL4 PRIMARY KEY,
+  value INT4,
+  merchant_id INT4 REFERENCES merchants(id),
+  tag_id INT4 REFERENCES tags(id)
+);
