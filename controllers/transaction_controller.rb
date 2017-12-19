124876c47021c2ab3ad2ea248bbaae32f182124c
Amy <amy.jeanette.morrson@gmail.com>
2017-12-19 14:29:39 +0000

    added delete function for transaction belonging to user

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -30,7 +30,7 @@ get('/:id/transactions/new') do
   erb(:'transactions/new')
 end
 
-get('/:account_id/transactions/:transaction_id/show') do
+get('/:account_id/transactions/:transaction_id') do
   @transaction = Transaction.find_by_id(params['account_id'].to_i, params['transaction_id'].to_i)
   @account_id = params['account_id'].to_i
   erb(:'transactions/show')
@@ -40,11 +40,25 @@ end
 get('/:account_id/transactions/:transaction_id/edit') do
   @transaction = Transaction.find_by_id(params['account_id'].to_i, params['transaction_id'].to_i)
   @account_id = params['account_id'].to_i
-  p @transaction
   @merchant = @transaction.merchant
   @merchants = Merchant.all
   @tags = Tag.all
   erb(:'transactions/edit')
+end
+
+post ('/:account_id/transactions/:transaction_id') do # update
+  @transaction = Transaction.new(params)
+  p params[:id]
+  p @transaction
+  @transaction.save
+  redirect('/:account_id/transactions')
+end
+
+post('/:account_id/transactions/:transaction_id/delete') do
+  @transaction = Transaction.find_by_id(params['account_id'].to_i, params['transaction_id'].to_i)
+  p@transaction
+  @transaction.delete()
+  redirect to "/#{@transaction.account_id}/transactions"
 end
 
 # post ('/transactions/:id') do
--- models/transaction.rb
+++ models/transaction.rb
@@ -38,6 +38,12 @@ class Transaction
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM transactions WHERE id=$1;"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def merchant()
     merchant = Merchant.find(@merchant_id)
   end
