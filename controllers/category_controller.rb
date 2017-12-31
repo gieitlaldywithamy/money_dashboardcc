ab1e3577d5369d4cd96a633c8f1aa8e05a67c457
Amy <amy.jeanette.morrson@gmail.com>
2017-12-31 19:44:59 +0000

    removing unnecessary files

--- controllers/category_controller.rb
+++ controllers/category_controller.rb
@@ -2,9 +2,6 @@ require 'pry-byebug' if development?
 require 'sinatra'
 require('sinatra/contrib/all') if development?
 
-require_relative('merchant_controller.rb')
-require_relative('category_controller.rb')
-
 require_relative('../models/transaction.rb')
 require_relative('../models/category.rb')
 require_relative('../models/merchant.rb')
